8658b78e55a03256743e7b552b20361547eab4e0
Ross-Martin5 <rmartin.ross5@gmail.com>
2022-04-08 04:23:34 -0500

    Migrated TableFiles and added Cars App

---
+++ cars_project/local_setting.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-7i!^nrx8=#erh7b4r^m_@$$0=b!o!pq6n2e+tjw6t&rhe#&11)'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'S0ccer05!1'
+    }
+}
